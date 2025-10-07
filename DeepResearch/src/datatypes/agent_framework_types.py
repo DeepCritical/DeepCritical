25b544ad504396b1e78c67254fe5a355c1bae79c
Joseph Pollack <joseph@legml.ai>
2025-10-07 13:19:35 +0200

    improves readme based on discussion

--- DeepResearch/src/datatypes/agent_framework_types.py
+++ DeepResearch/src/datatypes/agent_framework_types.py
@@ -91,3 +91,4 @@ __all__ = [
     # Options types
     "ChatOptions",
 ]
+
--- DeepResearch/src/datatypes/agent_framework_usage.py
+++ DeepResearch/src/datatypes/agent_framework_usage.py
@@ -100,3 +100,4 @@ class UsageDetails(BaseModel):
             and self.total_token_count == other.total_token_count
             and self.additional_counts == other.additional_counts
         )
+
